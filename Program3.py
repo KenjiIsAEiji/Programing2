deb56402284894f5f0acc5d1ebff89eb4ab6c984
eijiMiyazawa <b1800722@planet.kanazawa-it.ac.jp>
2018-11-19 12:47:25 +0900

    レポート用のワードファイル

--- Program3.py
+++ Program3.py
@@ -2,10 +2,10 @@ def Txt_output():
 
     writeData = str()
     
-    with open("C:\C\Program3.py") as file:
+    with open("C:\prog2\Program3.py") as file:
         data = file.read().split('\n')
         for i in range(len(data)):
             writeData = writeData + "%04d"%(i + 1) + " " + data[i] + "\n"
     
-    with open("C:\C\OutPut.txt",mode="w") as newFile:
+    with open("C:\prog2\output.txt",mode="w") as newFile:
         newFile.write(writeData)
